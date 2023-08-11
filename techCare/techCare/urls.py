761aac48f4a5747b7381c4d7fa4b30c262d818f7
Eniola Hannah <heniolahannah@gmail.com>
2023-08-11 12:25:20 +0100

    I created a new folder called registration for all of my signing-in options

--- techCare/techCare/urls.py
+++ techCare/techCare/urls.py
@@ -5,4 +5,8 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', TemplateView.as_view(template_name = 'index.html'), name='home'),
+    path('doctors/', TemplateView.as_view(template_name='doctors.html'), name='doctors'),
+    path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
+    path('contact/', TemplateView.as_view(template_name='contact.html'), name='contact'),
+    path('blog/', TemplateView.as_view(template_name='blog.html'), name='blog'),
 ]
