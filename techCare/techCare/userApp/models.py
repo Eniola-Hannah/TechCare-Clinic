4c6f72b038544f831cb7351abc3de1664b3faeed
Eniola Hannah <heniolahannah@gmail.com>
2023-08-14 13:10:09 +0100

    i imported all important links to the models.py of userApp

--- techCare/techCare/userApp/models.py
+++ techCare/techCare/userApp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+# The two line of code below is to make the table under the same with the database
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
