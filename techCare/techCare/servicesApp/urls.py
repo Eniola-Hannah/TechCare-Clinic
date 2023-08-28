1435402c645177182548df44c33f385e6b270f40
Eniola Hannah <heniolahannah@gmail.com>
2023-08-28 13:56:16 +0100

    I added the nav link of both patient booking and my booking in the common page with the url link at urls.py

--- techCare/techCare/servicesApp/urls.py
+++ techCare/techCare/servicesApp/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     re_path(r'^display_service/(?P<display>\w+)/', sw.displayServices, name="display_service"), 
     re_path(r'^edit_service/(?P<serv_id>\d+)/', sw.editServices, name='edit_service'),
     re_path(r'^service_details/(?P<serv_id>\d+)/', sw.serviceDetails, name='service_details'),
+    re_path(r'^my_booking/(?P<user_id>\d+)/', sw.myBooking, name='my_booking'),
+    re_path(r'^patient_booking/(?P<user_id>\d+)/', sw.patientBooking, name='patient_booking'),
 
 ]
