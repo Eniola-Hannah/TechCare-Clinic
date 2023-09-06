7f6d6315a6e7dbd6e4babf24304c7cb3f998390c
Eniola Hannah <heniolahannah@gmail.com>
2023-09-06 13:04:23 +0100

    I created the models.py for payment App

--- techCare/techCare/paymentApp/models.py
+++ techCare/techCare/paymentApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from techCare.userApp.models import Profile
+from techCare.servicesApp.models import BookingService
 
 # Create your models here.
+class Payment_service(models.Model):
+    payment_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(Profile, null=False, unique=False, on_delete=models.CASCADE)
+    booking = models.ForeignKey(BookingService, null=False, unique=False, on_delete=models.CASCADE)
+    amount = models.BigIntegerField(unique=False, null=False)
+    date_of_payment = models.DateTimeField(auto_now_add=True, null=False)
+    paystack_detail = models.BigIntegerField(unique=True, null=False)
